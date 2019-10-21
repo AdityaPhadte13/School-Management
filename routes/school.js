d876a2c16dafbc9158abda379e869d20c8eba758
Aditya Phadte <adityaphadte13@gmail.com>
2019-10-21 23:50:29 +0530

    added 404 page and path util

--- routes/school.js
+++ routes/school.js
@@ -1,18 +1,19 @@
+const rootPath = require('../util/path')
 const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
-router.get(['/','/home'], (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+router.get(['/', '/home'], (req, res) => {
+    res.sendFile(path.join(rootPath, 'views', 'index.html'));
 })
 
 router.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'about.html'));
+    res.sendFile(path.join(rootPath, 'views', 'about.html'));
 })
 
 router.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'contact.html'));
+    res.sendFile(path.join(rootPath, 'views', 'contact.html'));
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,12 +12,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(schoolRouter)
-app.use('/student',studentRouter);
-app.use('/teacher',teacherRouter);
-app.use('/admin',adminRouter);
+app.use('/student', studentRouter);
+app.use('/teacher', teacherRouter);
+app.use('/admin', adminRouter);
 
 app.use((req, res, next) => {
-	res.status(404).send("<h1>404 Page Not Found</h1>");
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
