49070e11042f7e050b97da2efcfbd8c0df602f5c
Aditya Phadte <adityaphadte13@gmail.com>
2019-10-22 21:32:58 +0530

    added static access(404.css)

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('view engine' ,'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(schoolRouter)
 app.use('/student', studentRouter);
@@ -19,7 +20,10 @@ app.use('/teacher', teacherRouter);
 app.use('/admin', adminRouter);
 
 app.use((req, res, next) => {
-	res.status(404).render('404',{ pageTitle: "Page Not Found"});
+	res.status(404).render('404',{ 
+		pageTitle: "Page Not Found",
+		path: ''
+	});
 })
 
 
