bcc3cee60aa1fe36c0ccfe7a0cb7fd3e9916186f
Aditya Phadte <adityaphadte13@gmail.com>
2019-10-23 08:46:45 +0530

    Login Forms WORKING

--- routes/student.js
+++ routes/student.js
@@ -5,6 +5,7 @@ const studentController = require("../controllers/student");
 const router = express.Router();
 
 router.get("/login", studentController.getStudentLogin);
+router.post("/login", studentController.postStudentLogin);
 
 router.get(["/", "/home"], studentController.getStudentHome);
 
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
