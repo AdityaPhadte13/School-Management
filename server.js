dded0a1c29e7fe0b165479642f66b6c7dd4076cb
Omkar Patil <pomkar114@gmail.com>
2019-10-24 13:30:52 +0530

    added database connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require('./util/database');
 
 const schoolRouter = require("./routes/school");
 const studentRouter = require("./routes/student");
@@ -8,6 +9,14 @@ const teacherRouter = require("./routes/teacher");
 const adminRouter = require("./routes/admin");
 
 const errorController = require("./controllers/error");
+
+db.execute('select * from dept;')
+.then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 app.set("view engine", "ejs");
