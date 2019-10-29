430e6587a88445ba987518778db82686862af4cf
Omkar Patil <pomkar114@gmail.com>
2019-10-29 10:21:40 +0530

    connecte routes

--- routes/staff.js
+++ routes/staff.js
@@ -12,6 +12,7 @@ const router = express.Router();
 
 router.get(["/", "/home"], isAuth, schoolController.getHome);
 
+//staff routers
 router.get("/staffData", isAuth, isAdmin, staffController.getStaffData);
 router.post(
   "/staffData",
@@ -28,6 +29,7 @@ router.post(
 router.get("/staffData/add", staffController.getStaffDataAdd)
 router.get("/staffdata/view/:id",staffController.getStaffDataView);
 
+//teacher routers
 router.get("/teacherData", isAuth, isAdmin, staffController.getTeacherData);
 router.post(
   "/teacherData",
@@ -41,7 +43,9 @@ router.post(
   isAdmin,
   staffController.postTeacherData
 );
-
+router.get("/teacherdata/view/:id",staffController.getTeacherDataView);
+router.get("/teacherData/add", staffController.getTeacherDataAdd)
+//student routers
 router.get("/studentData", isAuth, isAdmin, staffController.getStudentData);
 router.post(
   "/studentData",
@@ -56,8 +60,9 @@ router.post(
   staffController.postStudentData
 );
 
-
-
+router.get("/studentdata/view/:id",staffController.getStudentDataView);
+router.get("/studentData/add", staffController.getStudentDataAdd);
+//school routers
 router.get("/about", isAuth, schoolController.getAbout);
 router.get("/contact", isAuth, schoolController.getContact);
 
